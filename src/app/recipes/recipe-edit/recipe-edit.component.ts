2a9b16c65780db2e845ea952b4deb8f10637be15
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2021-11-29 00:38:08 +0530

    Added button function

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,7 +13,10 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editmode: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  recipeForm: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -21,6 +26,24 @@ export class RecipeEditComponent implements OnInit {
           this.editmode = params['id'] !=null;
         }
       )
+  }
+
+  private initForm(){
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDesc = '';
+
+    if(this.editmode){
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDesc = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDesc),
+    })
   }
 
 }
